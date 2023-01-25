b4382de9414ee6f33a8bf286384876739f9d48db
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-01-26 08:19:36 +0900

    add append command

---
+++ cmd_append.go
@@ -0,0 +1,25 @@
+package shell
+
+import "github.com/chzyer/readline"
+
+func init() {
+	RegisterCmd(&Cmd{
+		Name:    "append",
+		Aliases: []string{},
+		PcFunc:  pcAppend,
+		Action:  doAppend,
+		Desc:    "append table",
+		Usage: `  append [options]
+  options:
+    --input, -i   input file, (default: '-' stdin)
+    <<EOF         EOF mark, use any string matches [a-zA-Z0-9]+ for "EOF"`,
+	})
+}
+
+func pcAppend(cc Client) readline.PrefixCompleterInterface {
+	return readline.PcItem("append")
+}
+
+func doAppend(cc Client, sqlText string, interactive bool) {
+
+}
