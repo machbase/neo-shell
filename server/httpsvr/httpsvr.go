c1fa8910632494511c2330d6cb1f1fbef7f3f0d8
Kwon, Yeong Eon <eirny.kwon@machbase.com>
2023-02-20 17:00:16 +0900

    http client auth

--- server/httpsvr/httpsvr.go
+++ server/httpsvr/httpsvr.go
@@ -1,12 +1,17 @@
 package httpsvr
 
 import (
+	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 	logging "github.com/machbase/neo-logging"
 	spi "github.com/machbase/neo-spi"
 )
+
+type AuthServer interface {
+	ValidateClientToken(token string) (bool, error)
+}
 
 func New(db spi.Database, conf *Config) (*Server, error) {
 	return &Server{
@@ -29,6 +34,8 @@ type Server struct {
 	conf *Config
 	log  logging.Log
 	db   spi.Database
+
+	authServer AuthServer // injection point
 }
 
 func (svr *Server) Start() error {
@@ -38,7 +45,14 @@ func (svr *Server) Start() error {
 func (svr *Server) Stop() {
 }
 
+func (svr *Server) SetAuthServer(authServer AuthServer) {
+	svr.authServer = authServer
+}
+
 func (svr *Server) Route(r *gin.Engine) {
+	if svr.authServer != nil {
+		r.Use(svr.handleAuthToken)
+	}
 	for _, h := range svr.conf.Handlers {
 		prefix := h.Prefix
 		// remove trailing slash
@@ -59,5 +73,36 @@ func (svr *Server) Route(r *gin.Engine) {
 			r.POST(prefix+"/write", svr.handleWrite)
 			r.POST(prefix+"/write/:table", svr.handleWrite)
 		}
+	}
+}
+
+func (svr *Server) handleAuthToken(ctx *gin.Context) {
+	if svr.authServer == nil {
+		ctx.JSON(http.StatusUnauthorized, map[string]any{"success": false, "reason": "no auth server"})
+		ctx.Abort()
+	}
+	auth, exist := ctx.Request.Header["Authorization"]
+	if !exist {
+		ctx.JSON(http.StatusUnauthorized, map[string]any{"success": false, "reason": "missing authorization header"})
+		ctx.Abort()
+	}
+	found := false
+	for _, h := range auth {
+		if !strings.HasPrefix(strings.ToUpper(h), "BEARER ") {
+			continue
+		}
+		tok := h[7:]
+		result, err := svr.authServer.ValidateClientToken(tok)
+		if err != nil {
+			svr.log.Errorf("server private key", err)
+		}
+		if result {
+			found = true
+			break
+		}
+	}
+	if !found {
+		ctx.JSON(http.StatusUnauthorized, map[string]any{"success": false, "reason": "missing valid token"})
+		ctx.Abort()
 	}
 }
